d464577f41179c8b4bc045b1dd08fcc7d1874837
Lucas Takanori <git config --global user.email lucas.takanori.sanchez@estudiantat.upc.edu>
2022-10-19 09:28:59 +0200

    initialized sensor data

--- app/src/main/java/com/example/datawithgraph/MainActivity.java
+++ app/src/main/java/com/example/datawithgraph/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.datawithgraph;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_currentAccel;
+    ProgressBar prog_shakeMeter;
+    private  SensorManager mSensorManager;
+    private  Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_currentAccel = findViewById(R.id.txtAccel);
+        prog_shakeMeter = findViewById(R.id.prog_shakeMeter);
+
+        //initialize sensor data
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 }
