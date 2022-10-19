698e03344d7729b795d80c1058e6d654af03a73b
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2022-10-19 11:11:43 +0200

    accel value

--- app/src/main/java/com/example/datawithgraph/MainActivity.java
+++ app/src/main/java/com/example/datawithgraph/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.datawithgraph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -14,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar prog_shakeMeter;
     private  SensorManager mSensorManager;
     private  Sensor mAccelerometer;
+
+    private double accelerationCurrentValue;
+    private double accelerationPreviousValue;
+    private double changeInAcceleration;
+
+    private  SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            accelerationCurrentValue = Math.sqrt(x * x + y * y + z * z );
+            accelerationPreviousValue = accelerationCurrentValue;
+            changeInAcceleration = Math.abs(accelerationCurrentValue - accelerationPreviousValue);
+
+            //uptdate text views
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +52,14 @@ public class MainActivity extends AppCompatActivity {
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+    }
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(sensorEventListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(sensorEventListener);
     }
 }
